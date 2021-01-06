862c81d5a29ea37c454e0221701ba143f0c0234f
Maid Bešlagić <maid.beslagic@edu.fit.ba>
2021-01-06 17:48:25 +0100

    Created Korisnici controller, add some extensions, modify API service, add gitignore

---
+++ SmartRead.API/Controllers/KorisniciController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartRead.API.Services;
+
+namespace SmartRead.API.Controllers
+{
+    [Route("api/korisnici")]
+    [ApiController]
+    public class KorisniciController : ControllerBase
+    {
+        private readonly IKorisniciService _service;
+
+        public KorisniciController(IKorisniciService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public ActionResult<Model.Korisnik> Login([FromQuery] string username, [FromQuery] string password)
+        {
+            return _service.Autentifikacija(username, password);
+        }
+    }
+}
---
+++ SmartRead.WinUI/Helpers/APIService.cs
@@ -0,0 +1,112 @@
+﻿using Flurl.Http;
+using SmartRead.Model.Helpers;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartRead.WinUI.Helpers
+{
+    public class APIService
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+
+        private string _route = null;
+        public APIService(string route)
+        {
+            _route = route;
+        }
+
+        public async Task<T> Get<T>(object search)
+        {
+            var url = $"{Properties.Settings.Default.APIUrl}/{_route}";
+
+            try
+            {
+                if (search != null)
+                {
+                    url += "?";
+                    url += await search.ToQueryString();
+                }
+
+                return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                if (ex.Call.HttpStatus == System.Net.HttpStatusCode.Unauthorized)
+                {
+                    MessageBox.Show("You are not authentificated", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                throw;
+            }
+        }
+
+        public async Task<T> GetById<T>(object id)
+        {
+            var url = $"{Properties.Settings.Default.APIUrl}/{_route}/{id}";
+
+            return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+        }
+
+        public async Task<T> Insert<T>(object request)
+        {
+            var url = $"{Properties.Settings.Default.APIUrl}/{_route}";
+
+            try
+            {
+                return await url.WithBasicAuth(Username, Password).PostJsonAsync(request).ReceiveJson<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                MessageBox.Show(stringBuilder.ToString(), "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return default(T);
+            }
+        }
+
+        public async Task<T> Update<T>(object id, object request)
+        {
+            var url = $"{Properties.Settings.Default.APIUrl}/{_route}/{id}";
+
+            try
+            {
+                return await url.WithBasicAuth(Username, Password).PutJsonAsync(request).ReceiveJson<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errors = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
+
+                var stringBuilder = new StringBuilder();
+                foreach (var error in errors)
+                {
+                    stringBuilder.AppendLine($"{error.Key}, ${string.Join(",", error.Value)}");
+                }
+
+                MessageBox.Show(stringBuilder.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return default(T);
+            }
+        }
+
+        public async Task<T> Login<T>(object search)
+        {
+            var url = $"{Properties.Settings.Default.APIUrl}/{_route}/getAdmin";
+
+            if (search != null)
+            {
+                url += "?";
+                url += await search.ToQueryString();
+            }
+
+            return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+        }
+    }
+}
+
